e79d773f2d5edac352058e83d34e2da1db8e8214
Tabitha2005 <t.kuir@alustudent.com>
2026-09-11 17:23:16 +0100

    Create MyComponent with heading and paragraph, styled with CSS

---
+++ src/MyComponent.tsx
@@ -0,0 +1,12 @@
+import './MyComponent.css'
+
+const MyComponent: React.FC = () => {
+  return (
+    <div className="my-component">
+      <h1>My Component</h1>
+      <p>This is a simple React component.</p>
+    </div>
+  )
+}
+
+export default MyComponent
